807a00f38e295a7671bd75acdbca3c742ba10bd1
AnanyaM181 <ananyamohapatra822@gmail.com>
2025-09-26 23:58:27 +0530

    Express API with GET /handleSum to calculate sum.

---
+++ week2/2.12/index04.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+function calculateSum(counter) {
+    var sum = 0;
+    for (var i =0 ; i < counter ; i++) {
+        sum = sum + i;
+    }
+    return sum;
+}
+
+function handleFirstRequest(req , res) {
+    var counter = req.query.counter;
+    var calculatedSum = calculateSum(counter);
+    
+    var answer = "The sum is " + calculatedSum;
+    res.send(answer);
+}
+
+app.get('/handleSum' , handleFirstRequest)
+
+function started() {
+    console.log (`Example app listening on port ${port}`)
+}
+
+app.listen(port, started)
+
+// GET , PUT , POST , DELETE
+// We mostly can do everyting by using GET method.
