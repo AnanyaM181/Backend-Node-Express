42f65a3415fe353be13f0ca6f6e5a670f1f62fcc
AnanyaMohapatra18 <ananyamohapatra822@gmail.com>
2025-12-14 00:58:19 +0530

    Added cors.

--- week3/3.3-urlSaver/index.js
+++ week3/3.3-urlSaver/index.js
@@ -1,13 +1,11 @@
 require('dotenv').config();
 const express = require('express');
-const cors = require('cors');
 const connectDb = require('./src/db/connectDb');
 const urlRoutes = require('./src/routes/urlRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
