10aeb9d1cbf3e7a4d211a3ba6ab60144e4669309
AnanyaMohapatra18 <ananyamohapatra822@gmail.com>
2025-12-16 00:15:43 +0530

    Add detailed comments to urlSaver model
    Expanded inline documentation in urlSaver.js to clarify schema fields, validation logic, and model export. Also updated the model name from 'Url' to 'Urls' for consistency.

--- week3/3.3-urlSaver/src/models/urlSaver.js
+++ week3/3.3-urlSaver/src/models/urlSaver.js
@@ -7,23 +7,42 @@ const validator = require('validator');
 // Without this, We would have to write our own URL-checking logic.
 
 const urlSchema = new mongoose.Schema({
-  url: {
-    type: String,
-    required: [true, 'URL is required'],
-    validate: {
+  /**
+A schema defines the structure of a document in MongoDB
+A blueprint / form that tells MongoDB what fields are allowed and what rules they follow
+  **/
+  url: { // This field will store the actual website link
+    type: String, // Tells MongoDB that url must be a string
+    required: [true, 'URL is required'], // If user doesn’t provide a URL, MongoDB throws an error
+    validate: { // Adds custom validation to the url field, Runs before saving data to database
       validator: (v) => validator.isURL(v, { protocols: ['http', 'https'], require_protocol: true }),
+      /*
+      v represents the value entered for url
+      validator.isURL() checks if v is a valid URL
+      Options used:
+      protocols: ['http', 'https']
+      → Only allow http or https URLs
+      require_protocol: true
+      → URL must include http:// or https://
+      */
       message: 'Invalid URL format (must include http:// or https://)',
+      // Custom error message shown if URL validation fails
     },
   },
-  description: {
-    type: String,
-    required: [true, 'Description is required'],
+  description: { // description is Used to store explanation about the URL
+    type: String, // Description must be a string
+    required: [true, 'Description is required'], // description mandatory
   },
-  createdAt: {
-    type: Date,
-    default: Date.now,
+  createdAt: { // Declares a field to store time when URL was saved
+    type: Date, // Field will store a date value
+    default: Date.now, // Automatically sets current date & time when data is saved
   },
 });
 
-const Url = mongoose.model('Url', urlSchema);
+const Url = mongoose.model('Urls', urlSchema);
+// Creates a model named Url
+// Links schema with MongoDB collection
+
 module.exports = Url;
+//Makes this model available to other files
+//Allows routes to use it for database operations
