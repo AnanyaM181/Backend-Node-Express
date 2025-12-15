63a6b5e50135233caab4bd7ae1aab9b610754303
AnanyaMohapatra18 <ananyamohapatra822@gmail.com>
2025-12-15 23:03:16 +0530

    Add explanatory comments to connectDb.js
    Added detailed comments to the connectDb.js file to clarify the purpose of each code block and improve code readability. No functional changes were made.

--- week3/3.3-urlSaver/src/db/connectDb.js
+++ week3/3.3-urlSaver/src/db/connectDb.js
@@ -1,18 +1,20 @@
 const mongoose = require('mongoose'); //Imports the Mongoose library
 // Mongoose is used to connect Node.js with MongoDB
 
-const connectDb = async () => {
-    try {
+const connectDb = async () => { // async allows us to use await inside the function
+    try { // If any error occurs in try, execution jumps to catch
         await mongoose.connect('mongodb+srv://ananyamohapatra822_db_user:Ananya%40183011@cluster0.kos3lm6.mongodb.net/test', {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
-        console.log('connect to mongoDb');
+        console.log('connect to mongoDb'); // Runs only if the connection is successful
 
-    } catch (error) {
+    } catch (error) { // Runs only if something goes wrong in the try block
         console.error('mongoDb connection failed', error.message);
 
     }
 
 }
 module.exports = connectDb;
+// Exports the connectDb function
+// Allows other files (like index.js) to import and use it
