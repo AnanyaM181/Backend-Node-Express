d57fea2f50623d9feb6f162f92ac1bdf4c4e3807
AnanyaMohapatra18 <ananyamohapatra822@gmail.com>
2025-12-22 00:59:58 +0530

    Add CORS middleware to Express app
    Integrated the 'cors' package and enabled CORS for the backend to allow cross-origin requests. This is necessary for frontend-backend communication during development and production.

--- week3_urlSaverFrontend/backend/index.js
+++ week3_urlSaverFrontend/backend/index.js
@@ -2,10 +2,12 @@ require('dotenv').config();
 const express = require('express');
 const connectDb = require('./src/db/connectDb');
 const urlRoutes = require('./src/routes/urlRoutes');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 5001;
 
+app.use(cors())
 app.use(express.json());
 
 // Connect to MongoDB
